3ecc18d25f7b5a39e18824ca61ec47f973c3adfd
asaf <asafkarnieli@gmail.com>
2018-06-06 23:58:24 +0300

    forgot this file...

---
+++ ex3_info_retrieval/src/ex3_info_retrieval/Config.java
@@ -0,0 +1,10 @@
+package ex3_info_retrieval;
+
+public class Config {
+	public static boolean improved_algo ;
+	public static String 		queryFile;
+	public static String 		docsFile;
+	public static String 		outputFile;
+	public static String 		retrievalAlgorithm;
+	public static int running_output_query_index=1;
+}
